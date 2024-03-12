bd45282e43784742f3235f69c93553d9bb0d3125
PedroOrtega88 <pedroortegaalonso88@gmail.com>
2024-03-12 21:58:35 +0100

    -a

--- vite-project/useState/src/App.jsx
+++ vite-project/useState/src/App.jsx
@@ -1,27 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 
 function App() {
   const [name, setName] = useState("Sofía");
-  const [newName, setNewName] = useState('');
 
   const handleClick = (newTeacherName) => {
     setName(newTeacherName);
   };
 
   return (
+    <Fragment>
     <div>
-      <h2>Teacher Name: {name}</h2>
+      <h2>Nombre del profesor: {name}</h2>
       <ul>
         <li onClick={() => handleClick("Data")}>Data</li>
         <li onClick={() => handleClick("Reyes")}>Reyes</li>
         <li onClick={() => handleClick("Yolanda")}>Yolanda</li>
-        
       </ul>
     </div>
+    
+    </Fragment>
+
   );
 }
 
